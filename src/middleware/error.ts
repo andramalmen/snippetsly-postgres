e5943798325ea7216fabfd94403203ce47398d57
Andra Malmen <andra.malmen@tieto.com>
2021-02-20 22:23:58 +0200

    Add docker

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 
+// next is required for error middleware
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const errorHandler = (err: unknown, req: Request, res: Response, next: NextFunction) => {
     if (err) {
         console.error(err);
